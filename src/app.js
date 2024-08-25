28b7c5e19038f88ff5ad0aef477045dba9b838e0
Arion Samarxhiu <arionsamarxhiu32@gmail.com>
2024-08-25 14:52:55 +0200

    Small modifications

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import './db.js';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import listingRoutes from './routes/listingRoutes.js';
 import reviewRoutes from './routes/reviewRoutes.js';
@@ -10,11 +11,20 @@ import { RealEstateErrors } from './utils/ErrorHandler.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/listings', listingRoutes);
 app.use('/reviews', reviewRoutes);
 app.use('/reservations', reservationRoutes);
+app.get('/health', (req, res) => {
+  if (mongoose.connection.readyState === 1) {
+    res.status(200).end();
+  } else {
+    res.status(500).end();
+  }
+});
+
 app.use((err, req, res) => {
   if (err instanceof RealEstateErrors) {
     res.status(err.code);
@@ -25,14 +35,6 @@ app.use((err, req, res) => {
   }
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
-});
-
-app.get('/health', (req, res) => {
-  if (mongoose.connection.readyState === 1) {
-    res.status(200).end();
-  } else {
-    res.status(500).end();
-  }
 });
 
 app.listen(5000, () => {
